7d24dee798e9a49bf0b1bf410b9cf6510090dec4
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-02 08:13:53 -0800

    modify

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z') + 1):
-    if (c == ord('q') or c == ord('e')):
-        pass
-    print("{:c}".format(c), end="")
+    if (c != ord('q') and c != ord('e')):
+        print("{:c}".format(c), end="")
