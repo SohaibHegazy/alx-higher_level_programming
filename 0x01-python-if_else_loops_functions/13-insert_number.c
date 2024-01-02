d0be5625f517d5e4d18df188c028c7db3aca2ad8
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-02 13:52:38 -0800

    modify

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -32,7 +32,7 @@ listint_t *insert_node(listint_t **head, int number)
 		{
 			new->next = current->next;
 			current->next = new;
-			return (current);
+			return (new);
 		}
 		current  = current->next;
 	}
