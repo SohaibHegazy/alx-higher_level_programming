4dd185ffc4a341a2ad8153305003919c165be422
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-02 10:56:23 -0800

    modify

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for n in range(1, 101):
-        if n % 3 == 0 and n % 5 == 0 and n != 100:
+        if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz", end=" ")
         elif n % 3 == 0 and n % 5 != 0:
             print("Fizz", end=" ")
-        elif n % 3 != 0 and n % 5 == 0 and n != 100:
+        elif n % 3 != 0 and n % 5 == 0:
             print("Buzz", end=" ")
-        elif n == 100:
-            print("Buzz")
         else:
             print("{:d}".format(n), end=" ")
