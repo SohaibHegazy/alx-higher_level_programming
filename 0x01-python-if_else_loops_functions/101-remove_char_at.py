518d8386db75a134397cd172c45ed84e527fac71
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-02 15:03:21 -0800

    modify

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    cp_string = str[:n] + str[n + 1:]
+    if n < 0:
+        cp_string = str
+    else:
+        cp_string = str[:n] + str[n + 1:]
     return (cp_string)
