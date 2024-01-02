cbc61ea13ffac63c8bac95101089092de66f76aa
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-02 14:30:33 -0800

    modify

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -28,7 +28,7 @@ listint_t *insert_node(listint_t **head, int number)
 
 	while(current)
 	{
-		if (current->next->n > number || !current->next)
+		if (number < current->next-> || !current->next)
 		{
 			new->next = current->next;
 			current->next = new;
