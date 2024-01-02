fe711ead6a5e662681092e4589848d31d6056888
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-02 09:13:44 -0800

    modify

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-def uppercase(c):
-    if ord(c) >= ord('A') and ord(c) <= ord('Z'):
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
         return False
+
+def uppercase(str):
+    for c in str:
+        print("{:c}".format(ord(c) - 32 if islower else ord(c), end=""))
+print("")
