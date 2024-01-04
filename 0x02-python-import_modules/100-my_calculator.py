d378a3ddf17ee02681e8373d0008c4ba72303270
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-04 13:20:15 -0800

    modify

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    from calculator_1 import add, sub, mul, div
 
     arg_count = len(sys.argv) - 1
     a = int(sys.argv[1])
@@ -9,14 +10,14 @@ if __name__ == "__main__":
     if arg_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if op != "+" and op != "-" and op != "*" and op != "/":
+    if sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    if sys.argv[2] == "+":
-        print("{} + {} = {}".format(a, b, a + b))
-    elif sys.argv[2] == "-":
-        print("{} - {} = {}".format(a, b, a - b))
-    elif sys.argv[2] == "*":
-        print("{} * {} = {}".format(a, b, a * b))
-    elif sys.argv[2] == "/":
-        print("{} / {} = {}".format(a, b, a / b))
