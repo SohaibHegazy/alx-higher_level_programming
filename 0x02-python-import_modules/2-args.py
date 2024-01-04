184051bea55a146413f6f72596814f3f8f48f18f
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-04 11:20:57 -0800

    modify

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     arg_count = len(sys.argv) - 1
     if arg_count == 0:
         print("{} arguments.".format(arg_count))
-    elif arg_c == 1:
+    elif arg_count == 1:
         print("{} argument:".format(arg_count))
     else:
         print("{} arguments:".format(arg_count))
