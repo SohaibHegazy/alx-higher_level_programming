1c71beee1490ef353727b97ab68f5a1e9c6e9972
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-08 14:52:02 -0800

    modify

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,5 +8,5 @@ def print_matrix_integer(matrix=[[]]):
         if len(the_list) == 0:
             print()
         for n in range(len(the_list)):
-            print("{:d}".format(the_list[n]), 
+            print("{:d}".format(the_list[n]),
                   end="\n" if n is len(the_list) - 1 else " ")
