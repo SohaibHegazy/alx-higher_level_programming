77034aa6d5f5a8a24a40a9923f226db339380ab2
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-08 14:59:46 -0800

    modify

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if tuple_a[0] == 0:
+        n1 = 0
+    else:
+        n1 = tuple_a[0]
+
+    if tuple_a[1] == 0:
+        n2 = 0
+    else:
+        n2 = tuple_a[1]
+
+    if tuple_a[0] == 0:
+        n3 = 0
+    else:
+        n3 = tuple_a[0]
+
+    if tuple_a[1] == 0:
+        n4 = 0
+    else:
+        n4 = tuple_a[1]
+
+    return(n1 + n3, n2 + n4)
