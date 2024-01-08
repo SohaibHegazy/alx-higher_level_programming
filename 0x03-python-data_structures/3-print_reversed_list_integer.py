c6c957878599a3cee6f93c00259272ddede6ffea
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-08 13:44:00 -0800

    modify

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return None
     a = my_list.reverse
     for n in a:
         print("{:d}".format(n))
