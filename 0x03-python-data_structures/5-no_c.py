2293ab3b4e846d14e3f570b4d3c3c2c5071dba69
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-08 14:33:57 -0800

    modify

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    for c in my_string:
-        if c = 'c' or c = 'C':
-            del(c)
-    return my_string
+    new_string = ""
+    for c in range(len(my_string)):
+        if my_string[c] != 'c' and my_string[c] != 'C':
+            new_string += my_string[c]
+    return new_string
