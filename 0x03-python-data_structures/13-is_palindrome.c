ca09c8c9661d1532d2f93e78a7ac8fddb33201bb
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-08 16:00:41 -0800

    modify

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,6 +14,14 @@ int is_palindrome(listint_t **head)
 	return (pal(head, *head));
 }
 
+/**
+ * pal - the function
+ * @head: pointer to head
+ * @end: pointer to end
+ *
+ * Return: 1 if true and 0 if false
+ */
+
 int pal(listint_t **head, listint_t *end)
 {
 	if (end == NULL)
