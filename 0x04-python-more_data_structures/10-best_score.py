57f5164b97d779cd4135cb0fcee6e7989ac7042b
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-10 13:42:52 -0800

    modify

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    best = 0
+    best_k = ""
+    for k, v in a_dictionary.items():
+        if v > best:
+            best = v
+            best_k = k
+    return a_dictionary
