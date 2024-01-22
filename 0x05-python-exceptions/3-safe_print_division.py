510a2218916d5b528bba1ddd053cf6987fe102b1
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-22 05:31:59 -0800

    modify

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    res = 0
-
+    
     try:
         res = a / b
     except ZeroDivisionError:
         res = None
     finally:
-        print("Inside Result: {}".format(res))
+        print("Inside result: {}".format(res))
         return res
