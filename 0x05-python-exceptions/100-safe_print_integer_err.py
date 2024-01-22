ce3d0c3c573150aa8f33e3c5fe9914f9e72b0a16
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-22 08:02:00 -0800

    modify

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_integer_err(value):
+    bool = True
+
     try:
         print("{:d}".format(value))
-        return True
     except Exception as err:
         print("Exception: ", err, file=sys.stderr)
-        return False
+        bool = False
+
+    return bool
