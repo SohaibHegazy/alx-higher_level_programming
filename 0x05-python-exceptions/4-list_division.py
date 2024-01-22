e0215cb10e6721b6c3031e4f87aaafede779d6df
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-22 07:38:09 -0800

    modify

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             res = 0
-            continue
         except (ValueError, TypeError):
             print("wrong type")
             res = 0
