a8fed4acd3edf07e028969e2a74df11f289de9dd
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-22 07:55:06 -0800

    modify

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        raise (ValueError, TypeError) as err:
+            print("Exception: ", err)
+            return False
