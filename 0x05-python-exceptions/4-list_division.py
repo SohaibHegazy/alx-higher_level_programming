b22b37209fdbecd61bf9ca58627b63aae0ceef55
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-22 07:17:46 -0800

    modify

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
     while i is not list_length:
         try:
             res = my_list_1[i] / my_list_2[i]
-            continue
+            new_list.append(res)
         except ZeroDivisionError:
             print("division by 0")
             res = 0
