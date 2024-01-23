14e9478e24605d0a9c16cc94e52c612238125720
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-23 14:29:35 -0800

    modify

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" square class """
+
 
 class Square:
+    """ init """
     def __init__(self, size):
+        """ define square size as private attr """
         self.__size = size
