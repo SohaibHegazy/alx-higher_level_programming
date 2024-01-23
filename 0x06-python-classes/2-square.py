b3b621694f6f8553be31170f35a4f7235018b2ce
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-23 14:39:20 -0800

    modify

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,12 +5,12 @@
 class Square:
     """ init """
     def __init__(self, size=0):
-        """ check if < 0 """
-        if size < 0:
-            """ Raise ValueError """
-            raise ValueError("size must be >= 0")
         """ check if not integer """
         if not isinstance(size, int):
             """ raise TypeError """
             raise TypeError("size must be an integer")
+        """ check if < 0 """
+        if size < 0:
+            """ Raise ValueError """
+            raise ValueError("size must be >= 0")
         self.__size = size
