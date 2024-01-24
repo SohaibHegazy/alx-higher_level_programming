4e0f9710bd7cb92b832e0cd29b70f13d359f6eed
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-23 16:19:17 -0800

    modify

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -55,12 +55,12 @@ class Square:
             print("")
             return
 
-    for i in range(0, self.__position[1]):
-        print("")
+        for i in range(0, self.__position[1]):
+            print("")
 
-    for i in range(0, self.__size):
-        for j in range(0, self.__position[0]):
-            print(" ", end="")
-        for k in range(0, self.__size):
-            print("#", end="")
-        print("")
+        for i in range(0, self.__size):
+            for j in range(0, self.__position[0]):
+                print(" ", end="")
+            for k in range(0, self.__size):
+                print("#", end="")
+            print("")
