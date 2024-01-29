cf5a52ce8e809b9e7e49d2e39b186c9707d71ef0
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-29 12:18:05 -0800

    modify

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -56,6 +56,7 @@ class Rectangle:
 
     def __str__(self):
         """ draw the rectangle using # """
+        rec = ""
         if self.__width == 0 or self.__height == 0:
             rec = ""
         else:
