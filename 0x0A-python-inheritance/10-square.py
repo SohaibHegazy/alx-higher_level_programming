59d8b0dff82ab1d11a62bf412dcb9f5d8ddfc9e6
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-05 05:16:04 -0800

    modify

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" module for square as an inheret of Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class of square that inherets from Rectangle """
+    def __init__(self, size):
+        """ constructor of square """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ function to calculate area"""
+        return self.__size * self.__size
