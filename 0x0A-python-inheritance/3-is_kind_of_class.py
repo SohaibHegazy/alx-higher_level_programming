262070323f887bd7438145c049fb98edc1333b70
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-05 04:32:29 -0800

    modify

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" check the type Module """
+
+
+def is_same_class(obj, a_class):
+    """
+    function to check if type matches
+
+    Args:
+    obj: the object to compare
+    a_class: the class to compare to
+
+    Return: true if matches and false otherwise
+    """
+    return isinstance(obj, a_class)
