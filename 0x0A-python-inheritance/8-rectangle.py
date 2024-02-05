67504de2e747383837bbf976bbe69ef259296ac1
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-05 04:58:19 -0800

    modify

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,6 +8,5 @@ class Rectangle(BaseGeometry):
         """ initiate width and height """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
         self.__width = width
         self.__height = height
