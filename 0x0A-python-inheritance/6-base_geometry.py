4cfae0bf14a9ded535695b7b88be5324d3191a9f
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-05 04:40:37 -0800

    modify

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module for BaseGEometry class """
+
+
+class BaseGeometry:
+    """ class of BaseGeometry """
+    def area(self):
+        """
+        function that raises an exception
+
+        Args:
+        self
+
+        Return: Raise an exception
+        """
+        raise Exception("area() is not implemented")
