613e345721fc73c56c8cb40ec8597af3149361b6
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-07 05:18:57 -0800

    modify

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,12 +4,12 @@
 
 import sys
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+load_json = __import__('6-load_from_json_file.py').load_from_json_file
 
 arglist = list(sys.argv[1:])
 
 try:
-    exist_data = load_from_json_file('add_item.json')
+    exist_data = load_json('add_item.json')
 except Exception:
     exist_data = []
 
