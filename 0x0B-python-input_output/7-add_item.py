0a031b4a2c670486993d5cea8691359bc95084f0
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-07 06:12:24 -0800

    modify

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ arglist = list(sys.argv[1:])
 try:
     old_data = load_from_json_file('add_item.json')
 except Exception:
-    old_data = []
+    old_data = ()
 
 old_data.extend(arglist)
 save_to_json_file(old_data, 'add_item.json')
