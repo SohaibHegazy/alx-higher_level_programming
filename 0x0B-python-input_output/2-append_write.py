efb669722bb4e600bc097697221bf814072e0e5a
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-07 05:07:21 -0800

    modify

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -14,5 +14,5 @@ def append_write(filename="", text=""):
 
      return: number of characters added
      """
-     with open(filename, 'a', encoding="utf-8") as f:
+     with open(filename, "a", encoding='utf-8') as f:
          return f.write(text)
