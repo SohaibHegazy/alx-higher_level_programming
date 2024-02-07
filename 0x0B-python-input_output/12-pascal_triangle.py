b3949641be2c2730bc894a2b153014858cd1fe6a
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-07 05:16:12 -0800

    modify

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -14,7 +14,7 @@ def pascal_triangle(n):
     while len(p_tri) < n:
         tri = p_tri[-1]
         temp = [1]
-        for i in range(len(tri) -1):
+        for i in range(len(tri) - 1):
             temp.append(tri[i] + tri[i + 1])
         temp.append(1)
         p_tri.append(temp)
