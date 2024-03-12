0ff54ed2b87de16802314fab88dd027b9cd5b3c3
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-03-12 13:04:28 -0700

    modify

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records of the table second_table
-SELECT score, name FROM second_table WHERE name != NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name != "" ORDER BY score DESC;
