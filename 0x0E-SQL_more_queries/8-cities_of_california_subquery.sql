dd92153d72b91fb30d51cca3282a211a01dd0ff2
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-03-13 12:55:34 -0700

    modify

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- a script that lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT id, name FROM hbtn_0d_usa.cities
-WHERE hbtn_0d_usa.cities.state_id IN
-(SELECT id FROM hbtn_0d_usa.states
+SELECT id, name FROM cities
+WHERE state_id IN
+(SELECT id FROM states
 	WHERE name = "California")
-ORDER BY hbtn_0d_usa.cities.id;
+ORDER BY id;
