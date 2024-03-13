45e7c130e9332bde3f98878b84b5fbc920e95c30
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-03-13 14:42:57 -0700

    modify

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_genre.name, COUNT(tv_show_genres.show_id) AS n
+SELECT tv_genre.name, COUNT(*) AS n
 FROM INNER JOIN tv_genre, tv_show_genres
 ON tv_show_genres.genre_id = tv_genre.name
 GROUP BY tv_genre.name
