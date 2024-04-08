493f624ba056f0a130a1e1888f0309700efe182c
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-08 15:02:48 -0700

    modify

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const argList = process.argv.sort();
-  console.log(argList.reverse()[1]);
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
 }
