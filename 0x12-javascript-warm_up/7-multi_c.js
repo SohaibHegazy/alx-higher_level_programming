974baa4940950ba986c5441e77f844c09b1d96c2
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-08 13:47:30 -0700

    modify

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 // a script that prints x times “C is fun”
 
 if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('C is fun');
