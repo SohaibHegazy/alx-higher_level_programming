7b1b901219c808de799f11701d2e1cc0452bfaed
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-08 13:37:28 -0700

    modify

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,7 @@
 // a script that prints 3 lines using an array of string and a loop
 
 let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
 for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
 }
