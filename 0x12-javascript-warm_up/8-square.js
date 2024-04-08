f86b7937e16209075b8dafadd12be1ef2aedf02e
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-08 14:06:43 -0700

    modify

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,17 +4,17 @@
 let line;
 
 if (isNaN(process.argv[2])) {
-    console.log('Missing size');
-  } else {
-    for (let i = 0; i < parseInt(process.argv[2]); i++) {
-      line = null;
-      for (let j = 0; j < parseInt(process.argv[2]); j++) {
-        if (line === null) {
-		line = 'X';
-	} else {
-		line += 'X';
-	}
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    line = null;
+    for (let j = 0; j < parseInt(process.argv[2]); j++) {
+      if (line === null) {
+        line = 'X';
+      } else {
+        line += 'X';
       }
-      console.log(line);
     }
+    console.log(line);
+  }
 }
