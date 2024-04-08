71bf3676b9ccb425e3930b335c745a4b5c2de655
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-08 14:01:09 -0700

    modify

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
 // a script that prints a square
 
+let line;
+
 if (isNaN(process.argv[2])) {
     console.log('Missing size');
   } else {
     for (let i = 0; i < parseInt(process.argv[2]); i++) {
+      line = null;
       for (let j = 0; j < parseInt(process.argv[2]); j++) {
-        console.log('X');
+        if (line === null) {
+		line = 'X';
+	} else {
+		line += 'X';
+	}
       }
-      console.log('\n');
+      console.log(line);
     }
 }
