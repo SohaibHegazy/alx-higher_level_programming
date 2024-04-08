4ce35d93fb93f7d1852c5b87423672584af27995
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-08 14:27:52 -0700

    modify

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that computes and prints a factorial
+
+function getFactorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  } else {
+    return n * getFactorial(n - 1);
+  }
+}
+
+console.log(getFactorial(parseInt(process.argv[2])));
