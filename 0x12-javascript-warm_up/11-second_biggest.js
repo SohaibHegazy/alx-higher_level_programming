faa160cff7db5ff32831b89e1283ea78851e193f
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-08 14:50:59 -0700

    modify

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
 // searches the second biggest integer in the list of arguments
 
-let max = 0;
-let second_max = 0;
-
 if (!process.argv[3]) {
-  console.log(1);
+  console.log(0);
 } else {
-  for (let i = 2; i < process.argv.length; i++) {
-    if (parseInt(process.argv[i]) > max) {
-      second_max = max;
-      max = parseInt(process.argv[i]);
-    }
-  }
-  console.log(second_max);
+  const argList = process.argv.sort();
+  console.log(argList.reverse()[1]);
 }
