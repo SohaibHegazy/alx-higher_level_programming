79e63fe5c77c996cee71805d6044c027f3effed8
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-09 14:21:47 -0700

    modify

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
-    searchElement = list[i] ? count += 1 : count;
+    searchElement === list[i] ? count++ : count;
     return count;
   }
 }
