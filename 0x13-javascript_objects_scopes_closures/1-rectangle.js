e7ee3435aa6497c603e97fad52b913ef7da59499
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-09 08:05:27 -0700

    modify

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// an empty class Rectangle that defines a rectangle
+
+module.exports = class Rectangle {
+  width;
+  height;
+  
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
