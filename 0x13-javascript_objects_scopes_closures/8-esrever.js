944d21546c81baef9d73755ee0b75f8272f09d2f
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-09 14:29:52 -0700

    modify

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let revList = ();
+  for (let i = list.length, j = 0; i > 0; i--, j++) {
+    revList[j] = list[i - 1];
+  }
+  return revList;
+};
