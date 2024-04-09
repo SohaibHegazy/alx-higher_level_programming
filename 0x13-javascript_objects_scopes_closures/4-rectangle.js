4c7cffa9b8c43784ab9710285b400f2ea86b3cbf
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-09 08:34:26 -0700

    modify

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+// an empty class Rectangle that defines a rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < parseInt(this.height); i++) {
+      let line = null;
+      for (let j = 0; j < parseInt(this.width); j++) {
+        if (line === null) {
+          line = 'X';
+        } else {
+          line += 'X';
+        }
+      }
+      console.log(line);
+    }
+  }
+
+  rotate() {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double() {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
