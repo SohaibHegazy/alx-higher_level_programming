4ba4d20ce008ce70d2beb25ca2c6bcdb64292a35
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-09 08:54:37 -0700

    modify

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,16 +6,8 @@ module.exports = class Square extends Parent {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < parseInt(this.size); i++) {
-        let line = null;
-        for (let j = 0; j < parseInt(this.size); j++) {
-          if (line === null) {
-            line = 'c';
-          } else {
-            line += 'c';
-          }
-        }
-        console.log(line);
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
       }
     }
   }
