f6cdbc3b41ce50c0671271d5cd6f38cdd4488dc5
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-09 08:49:56 -0700

    modify

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Parent = require('./5-square.js');
+
+module.exports = class Square extends Parent {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < parseInt(this.size); i++) {
+        let line = null;
+        for (let j = 0; j < parseInt(this.size); j++) {
+          if (line === null) {
+            line = 'c';
+          } else {
+            line += 'c';
+          }
+        }
+        console.log(line);
+      }
+    }
+  }
+};
