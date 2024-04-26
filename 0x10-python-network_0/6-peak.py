a40f5f1af9a1ba639ae5443efb4e7cbfb7943dd2
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-04-26 14:15:35 -0700

    modify

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,7 +9,7 @@ def find_peak(list_of_integers):
     Function to find a peak of a list
     '''
 
-    if not list_of_integers:
+    if list_of_integers == []:
         return None
 
     list_len = len(list_of_integers)
